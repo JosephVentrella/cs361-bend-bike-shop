3426634f39fb87dabe66c8ac2178caa55f83edbc
Joseph Ventrella <ventrejo@oregonstate.edu>
2020-02-10 11:09:57 -0800

    Modified bike and main

--- bike.rb
+++ bike.rb
@@ -3,37 +3,55 @@
 class Bike
 
   STANDARD_WEIGHT = 200 # lbs
-  MAX_CARGO_ITEMS = 10
 
-  attr_accessor :id, :color, :price, :weight, :rented, :cargo_contents
+  attr_accessor :id, :color, :price, :weight, :cargo
 
-  def initialize(id, color, price, weight = STANDARD_WEIGHT, rented = false)
+  def initialize(id, color, price, weight = STANDARD_WEIGHT, cargo = [] )
     @id = id
     @color = color
     @price = price
     @weight = weight
-    @rented = rented
-    @cargo_contents = []
+    @cargo = Cargo.new(cargo)
   end
 
-  def rent!
-    self.rented = true
+
+end
+
+class Cargo
+
+    MAX_CARGO_ITEMS = 10
+
+  attr_accessor :contents
+
+  def initialize(cargo = [])
+    @contents = cargo
   end
 
-  def add_cargo(item)
-    self.cargo_contents << item
+  def add(item)
+    self.contents << item
   end
 
-  def remove_cargo(item)
-    self.cargo_contents.remove(item)
+  def remove(item)
+    self.contents.remove(item)
   end
 
-  def pannier_capacity
+  def capacity
     MAX_CARGO_ITEMS
   end
 
-  def pannier_remaining_capacity
-    MAX_CARGO_ITEMS - self.cargo_contents.size
+  def remaining_capacity
+    MAX_CARGO_ITEMS - self.contents.size
   end
 
 end
+
+class Rental
+
+  def initialize(bike)
+    @bike = bike
+  end
+
+end
+
+
+
