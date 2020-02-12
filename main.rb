929aeb7856f9cf65addfbf02c89751022c241e65
Joseph Ventrella <ventrejo@oregonstate.edu>
2020-02-12 12:01:48 -0800

    Refactored main, bike, luggage, rental

--- main.rb
+++ main.rb
@@ -4,10 +4,11 @@ require_relative 'bike'
 require_relative 'rental'
 
 items = [:apple, :water, :protein_bar]
+luggage = Luggage.new(Luggage::DEFAULT_MAX_CAPACITY, items)
 
-bike = Bike.new(1, :pink, 99.99, items)
+bike = Bike.new(1, :pink, 99.99)
 
-rental = Rental.new(bike)
+rental = Rental.new(bike, luggage)
 
 puts "Total Price: #{rental.price}, Total Weight: #{rental.weight}"
 
